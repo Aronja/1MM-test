fb2dc452b0d0508cec45c3db4fd8b9a9bb4d1b94
Aronja <anovicky@thoughtworks.com>
2018-07-02 17:05:18 +0100

    [MG/AN] local isolated build

--- build/server.js
+++ build/server.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '.')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.set('port', process.env.PORT || 8090);
 
 var server = app.listen(app.get('port'), function() {
