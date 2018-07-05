be4d2e4a44c5ef7ee2a5a2ce3607fca086f67cd7
Aronja <anovicky@thoughtworks.com>
2018-07-05 14:51:32 +0100

    send data from backend to frontend

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -2,7 +2,22 @@ import React from "react"
 
 export default class Hero extends React.Component {
 
+
+componentDidMount() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this)
+    }
+    };
+    xhttp.open("GET", "http://mentorapi-env.3rpp4bzma5.eu-west-2.elasticbeanstalk.com/greeting?name=Nas", true);
+    xhttp.send();
+  }
+
+
+
   render() {
+
     return (
       <div class="hero-text-box">
         <h1>
--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react','es2015','es2016','es2017','stage-0'],
-          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
-        }
-      }
-    ]
+context: path.join(__dirname, "src"),
+devtool: debug ? "inline-sourcemap" : false,
+entry: "./js/client.js",
+module: {
+loaders: [
+  {
+    test: /\.css$/,
+    loader: "style-loader!css-loader"
   },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "client.min.js"
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    loader: "file-loader"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+{
+test: /\.jsx?$/,
+exclude: /(node_modules|bower_components)/,
+loader: 'babel-loader',
+query: {
+presets: ['react','es2015','es2016','es2017','stage-0'],
+plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+}
+}
+]
+},
+output: {
+path: __dirname + "/src/",
+filename: "client.min.js"
+},
+plugins: debug ? [] : [
+new webpack.optimize.DedupePlugin(),
+new webpack.optimize.OccurrenceOrderPlugin(),
+new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+],
 };
